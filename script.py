eecbb8bcd74c7cf6b03272bcdd79961dc0bce206
guillaumerosin <guillaumerosin@users.noreply.github.com>
2026-03-02 17:08:07 +0100

    up

---
+++ script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import sys
+from collections import Counter
+
+class read:
+    all = []
+    def __init__(self,l):
+        c,s = l.split()
+        c = int(c)
+        self.c = c 
+        self.s = s
+        self.index = len(read.all)
+        read.all.append(self)
+
+    def amiinclude(me,other):
+        return other.s.count(me.s) 
+
+    def amiinter(me):
+        return [other for other in read.all if me.amiinclude(other) and me != other] 
+    
+
+filename = "rest2"
+
+for l in open(filename):read(l)
+print(len(read.all))
+
+for fr in read.all:
+    resultat = fr.test()
+    if resultat:print(fr.index,[x.index for x in resultat])
+
+
+        
+   
+
+# for seq in fr.test():
+#     fr in read.all < 1
+
+
+        
+   
