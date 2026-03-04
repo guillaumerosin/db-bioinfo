46cb7179e511959d5f0b5c12d21a500cc5568b40
David Coornaert <dcoorna@gmail.com>
2026-03-04 11:00:21 +0100

    up

--- script_coo.py
+++ script_coo.py
@@ -2,37 +2,46 @@
 import sys
 # ~ from collections import Counter
 try:fn=sys.argv[1]
-except fn=0
+except:fn=0
 class read:
-    all = []
-    def __init__(self,l):
-        c,s = l.split()
-        c = int(c)
-        self.c = c 
-        self.s = s
-        self.index = len(read.all)
-        read.all.append(self)
-
-    def amiinclude(me,other):
-        return other.s.count(me.s) 
-
-    def amiinter(me):
-        return [other for other in read.all if me.amiinclude(other) and me != other] 
-    
+	all = []
+	def __init__(self,l):
+		c,s = l.split()
+		c = int(c)
+		self.c = c 
+		self.s = s
+		self.l=len(s)
+		self.index = len(read.all)
+		read.all.append(self)
+	
+	def amiinclude(me,other):
+		return other.s.count(me.s) 
+	
+	def amiinter(me):
+		return [other for other in read.all if me.amiinclude(other) and me != other] 
+	
 
 for l in open(fn):read(l)
 print(len(read.all))
-
-for fr in read.all:
-    resultat = fr.test()
-    if resultat:print(fr.index,[x.index for x in resultat])
-
-
-        
-   
-
-# for seq in fr.test():
-#     fr in read.all < 1
+AR=list(read.all)
+AR.sort(key=lambda t:t.l)
+CLEAN=[]
+merged=unaffected=0
+while AR:
+	r=AR.pop(0)
+	resultat = r.amiinter()
+	if resultat:
+		print(f"todo : {len(AR)} | merging {r.index} seen {r.c} times  to {len(resultat)} reads")
+		tt=sum([other.c for other in resultat])
+		for other in resultat:
+			other.c+=r.c*other.c/tt
+		merged+=1
+	else:
+		print(f"todo : {len(AR)} | leaving {r.index} seen {r.c} times unmerged")
+		CLEAN.append(r)
+		unaffected+=1
+print("final read count",len(CLEAN))
+print(f"{merged} merged reads, {unaffected} untouched reads")
 
 
         
