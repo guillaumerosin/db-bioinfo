ab1c83fbbb5cd1050d024251f8f1d3cfd3f83bb8
David Coornaert <dcoorna@gmail.com>
2026-03-11 10:28:06 +0100

    up

--- script_coo.py
+++ script_coo.py
@@ -6,24 +6,44 @@ try:fn=sys.argv[1]
 except:fn=0
 class read:
 	all = []
+	km=10
+	KMR={}
 	def __init__(self,l):
 		c,s = l.split()
 		c = int(c)
 		self.c = c 
-		self.s = s
+		self.s = s.strip()
 		self.l=len(s)
 		self.index = len(read.all)
 		read.all.append(self)
-	
+		self.dkm()
 	def amiinclude(me,other):
 		return other.s.count(me.s) 
-	
 	def amiinter(me):
-		return [other for other in read.all[me.index+1:] if me.amiinclude(other)] 
-	
+		mfkm=me.s[:read.km]
+		valid=read.KMR[mfkm]
+		valid=[r for r in valid if r.l>me.l]
+		# ~ return [other for other in read.all[me.index+1:] if me.amiinclude(other)] 
+		return [other for other in valid if me.amiinclude(other)] 
+	def dkm(me):
+		k=read.km
+		ckm="_"+me.s[:k-1]
+		for c in me.s[k-1:]:
+			ckm=ckm[1:]+c
+			if ckm in read.KMR:
+				read.KMR[ckm].add(me)
+			else:
+				t=set()
+				t.add(me)
+				read.KMR[ckm]=t
 
 for l in open(fn):read(l)
-print(len(read.all))
+# ~ print(len(read.all))
+# ~ print(len(read.KMR))
+# ~ print(read.KMR.keys())
+# ~ print(list(read.KMR.keys())[:10])
+# ~ for k in list(read.KMR.keys())[:10]:
+	# ~ print(k,len(read.KMR[k]))
 read.all.sort(key=lambda t:t.l)
 for i,r in enumerate(read.all):r.index=i
 CLEAN=[]
